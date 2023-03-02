cdbefda3c326d1e3f4fbc93c268fc29b39b011ac
BhardwajArjit <iamarjitbhardwaj@gmail.com>
2023-03-02 13:31:08 +0530

    Complete gesture volume control

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -57,7 +57,7 @@ def main():
 
         currentTime = time.time()
         fps = 1 / (currentTime - prevTime)
-        pTime = currentTime
+        prevTime = currentTime
 
         cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
